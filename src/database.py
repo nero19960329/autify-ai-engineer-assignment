b7f1d0d1179ad2acf0bc2cc43663beb9fb91eb15
Zhao Wang <wangzhao2013tsinghua@gmail.com>
2024-05-11 04:26:19 +0900

    feat: sqlalchemy database url read from env

--- src/database.py
+++ src/database.py
@@ -1,7 +1,9 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///snippets.db"
+SQLALCHEMY_DATABASE_URL = os.environ["SQLALCHEMY_DATABASE_URI"]
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
