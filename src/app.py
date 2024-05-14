8c173e7ea3a3f9eb19295a6e57e45bc082f19798
nero19960329 <wangzhao2013tsinghua@gmail.com>
2024-05-14 18:46:29 +0900

    refactor: move js script out of index.html

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from src.database import engine
@@ -8,6 +9,7 @@ from src import models
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
 app.include_router(snippets.router, prefix="/api")
 app.include_router(generate.router, prefix="/api")
 app.include_router(run.router, prefix="/api")
